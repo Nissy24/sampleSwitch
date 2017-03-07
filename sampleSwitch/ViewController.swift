41a387a956d199afa8ebfc4781eae467ab807c8f
Nissy <nishisuji.0509@gmail.com>
2017-03-07 15:01:38 +0800

    Initial Commit

---
+++ sampleSwitch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampleSwitch
+//
+//  Created by 西筋啓人 on 2017/03/07.
+//  Copyright © 2017年 Hiroto Nishisuji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
